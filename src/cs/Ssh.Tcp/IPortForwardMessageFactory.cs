6d5e0770c1d90fed21c9ad9df101d32639317ef6
Jason Ginchereau <jasongin@microsoft.com>
2023-03-07 08:10:34 -1000

    Add more doc comments for interface methods

--- src/cs/Ssh.Tcp/IPortForwardMessageFactory.cs
+++ src/cs/Ssh.Tcp/IPortForwardMessageFactory.cs
@@ -18,18 +18,26 @@ public interface IPortForwardMessageFactory
 	/// <summary>
 	/// Creates a message for requesting to forward a port.
 	/// </summary>
+	/// <param name="port">The port number that is requested, or 0 if a random port is requested.
+	/// (The other side may choose a different port if the requested port is in use.)</param>
 	/// <returns>An instance or subclass of <see cref="PortForwardRequestMessage" />.</returns>
 	Task<PortForwardRequestMessage> CreateRequestMessageAsync(int port);
 
 	/// <summary>
 	/// Creates a message for a succesful response to a port-forward request.
 	/// </summary>
+	/// <param name="port">The port number that was requested by the other side. This may be
+	/// different from the local port that was chosen. Or if the other side requested a random
+	/// port then the actual chosen port number is returned in the success message.</param>
 	/// <returns>An instance or subclass of <see cref="PortForwardSuccessMessage" />.</returns>
 	Task<PortForwardSuccessMessage> CreateSuccessMessageAsync(int port);
 
 	/// <summary>
 	/// Creates a message requesting to open a channel for a forwarded port.
 	/// </summary>
+	/// <param name="port">The port number that the channel will connect to. All channel messages
+	/// use the originally requested port number, which may be different from the actual TCP socket
+	/// port number if the requested port was in use at the time of the forward request.</param>
 	/// <returns>An instance or subclass of <see cref="PortForwardChannelOpenMessage" />.</returns>
 	Task<PortForwardChannelOpenMessage> CreateChannelOpenMessageAsync(int port);
 }
--- src/ts/ssh-tcp/portForwardMessageFactory.ts
+++ src/ts/ssh-tcp/portForwardMessageFactory.ts
@@ -12,18 +12,26 @@ import { PortForwardSuccessMessage } from './messages/portForwardSuccessMessage'
 export interface PortForwardMessageFactory {
 	/**
 	 * Creates a message for requesting to forward a port.
+	 * @param port The port number that is requested, or 0 if a random port is requested.
+	 * (The other side may choose a different port if the requested port is in use.)
 	 * @returns An instance or subclass of `PortForwardRequestMessage`.
 	 */
 	createRequestMessageAsync(port: number): Promise<PortForwardRequestMessage>;
 
 	/**
 	 * Creates a message for a succesful response to a port-forward request.
+	 * @param port The port number that was requested by the other side. This may be different
+	 * from the local port that was chosen. Or if the other side requested a random port then
+	 * the actual chosen port number is returned in the success message.
 	 * @returns An instance or subclass of `PortForwardSuccessMessage`.
 	 */
 	createSuccessMessageAsync(port: number): Promise<PortForwardSuccessMessage>;
 
 	/**
 	 * Creates a message requesting to open a channel for a forwarded port.
+	 * @param port The port number that the channel will connect to. All channel messages use
+	 * the originally requested port number, which may be different from the actual TCP socket
+	 * port number if the requested port was in use at the time of the forward request.
 	 * @returns An instance or subclass of `PortForwardChannelOpenMessage`.
 	 */
 	createChannelOpenMessageAsync(port: number): Promise<PortForwardChannelOpenMessage>;
