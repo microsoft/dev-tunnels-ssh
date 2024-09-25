9529a867f7cd57f6f06a9aa09675eef0ebf1c2b2
Jason Ginchereau <jasongin@microsoft.com>
2024-09-25 12:27:43 -1000

    Fix libssl setup and detection on Mac OS

--- src/cs/Ssh/Algorithms/SshAlgorithm.cs
+++ src/cs/Ssh/Algorithms/SshAlgorithm.cs
@@ -84,7 +84,7 @@ public abstract class SshAlgorithm
 	[UnconditionalSuppressMessage(
 		"Trimming",
 		"IL2026:RequiresUnreferencedCode",
-		Justification = "Crypto types will no be trimmed because they're referenced elsewhere.")]
+		Justification = "Crypto types will not be trimmed because they're referenced elsewhere.")]
 #endif
 	private static Assembly? InitializeOpenSslAssembly()
 	{
@@ -100,46 +100,20 @@ public abstract class SshAlgorithm
 			return null;
 		}
 
-		// Try to P/Invoke into libssl 1.1, just to check whether it can be loaded. Otherwise
-		// the .NET OpenSSL initialization may abort the process when it fails to load libssl.
-		try
+		if (!CheckOpensslVersion())
 		{
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-			{
-				// On Mac, .NET prefers v1.1 but also supports v1.0.
-				try
-				{
-					_ = GetOpenSsl11VersionMac();
-				}
-				catch (DllNotFoundException)
-				{
-					try
-					{
-						_ = GetOpenSsl10VersionMac();
-					}
-					catch (EntryPointNotFoundException)
-					{
-						// The version_num entrypoint doesn't exist in older versions.
-						// That's fine -- this at least confirms the library was loaded.
-					}
-				}
-			}
-			else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-			{
-				// On Linux, .NET always prefers v1.1, but supports several other versions
-				// depending on the distro. Ignore other versions here to keep it simple.
-				_ = GetOpenSsl11VersionLinux();
-			}
-		}
-		catch (DllNotFoundException)
-		{
+			// Failed to load libssl. The library might be not installed or not found in the
+			// dynamic-library search path. On Mac OS, it may be necessary to set
+			// DYLD_FALLBACK_LIBRARY_PATH to a directory containing libssl*.dylib.
 			return null;
 		}
 
 		// Ensure the crypto interop is initialized. Unfortunately this relies on an
 		// internal type, but there's no better way to ensure OpenSSL is actually available
-		// before trying to really use it.
-		var interopCryptoType = opensslAssembly.GetType("Interop+Crypto");
+		// before trying to really use it. Depending on the .NET version, the type could be
+		// in either System.Security.Cryptography or System.Security.Cryptography.OpenSsl.
+		var interopCryptoType = typeof(SymmetricAlgorithm).Assembly.GetType("Interop+Crypto") ??
+			opensslAssembly.GetType("Interop+Crypto");
 		if (interopCryptoType == null)
 		{
 			// The internal interop type was not found. Since it's internal, it could get moved
@@ -161,6 +135,85 @@ public abstract class SshAlgorithm
 		return opensslAssembly;
 	}
 
+	/// <summary>
+	/// Try to P/Invoke the libssl version API, just to check whether it can be loaded. Otherwise
+	/// the .NET OpenSSL initialization may abort the process when it fails to load libssl.
+	/// </summary>
+	private static bool CheckOpensslVersion()
+	{
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+		{
+			// On Mac, .NET 8 supports OpenSSL v3, v1.1, and v1.0.
+			// Older .NET versions support v1.1 and v1.0.
+			if (Environment.Version.Major >= 8)
+			{
+				try
+				{
+					_ = GetOpenSsl3VersionMac();
+					return true;
+				}
+				catch (DllNotFoundException)
+				{
+				}
+			}
+
+			try
+			{
+				_ = GetOpenSsl11VersionMac();
+				return true;
+			}
+			catch (DllNotFoundException)
+			{
+			}
+
+			try
+			{
+				_ = GetOpenSsl10VersionMac();
+				return true;
+			}
+			catch (DllNotFoundException)
+			{
+			}
+			catch (EntryPointNotFoundException)
+			{
+				// The version_num entrypoint doesn't exist in v1.0.
+				// That's fine -- this at least confirms the library was loaded.
+				return true;
+			}
+		}
+		else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+		{
+			// On Linux, .NET 8 supports OpenSSL v3 and v1.1.
+			// Older .NET versions support v1.1.
+			if (Environment.Version.Major >= 8)
+			{
+				try
+				{
+					_ = GetOpenSsl3VersionLinux();
+					return true;
+				}
+				catch (DllNotFoundException)
+				{
+				}
+			}
+
+			try
+			{
+				_ = GetOpenSsl11VersionLinux();
+				return true;
+			}
+			catch (DllNotFoundException)
+			{
+			}
+		}
+
+		return false;
+	}
+
+	[DllImport("libssl.3.dylib", EntryPoint = "OpenSSL_version_num")]
+	[DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+	private static extern int GetOpenSsl3VersionMac();
+
 	[DllImport("libssl.1.1.dylib", EntryPoint = "OpenSSL_version_num")]
 	[DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
 	private static extern int GetOpenSsl11VersionMac();
@@ -168,6 +221,10 @@ public abstract class SshAlgorithm
 	[DllImport("libssl.1.0.0.dylib", EntryPoint = "OpenSSL_version_num")]
 	[DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
 	private static extern int GetOpenSsl10VersionMac();
+
+	[DllImport("libssl.so.3", EntryPoint = "OpenSSL_version_num")]
+	[DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+	private static extern int GetOpenSsl3VersionLinux();
 
 	[DllImport("libssl.so.1.1", EntryPoint = "OpenSSL_version_num")]
 	[DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
